1e94b70d3155b1817be4cac14b6a3cdca9c47a3d
2hys <tenjin62@gmail.com>
2024-02-01 18:08:08 +0900

    feat : 기초 UI 설정(sprite,size 미적용), ShopPanel 스크롤뷰 적용

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    // Scene이 바뀐다던가 해도 유지해야하는 데이터
+    public class Player
+    {
+        //이번 프로젝트에서 Player의 데이터가 저장될 필요는 없지만
+        //추후에는 Player의 Lv, name등 저장될 필요 있음.
+        //
+        public static float atk = 35;
+        public static float def = 40;
+        public static float hp = 100;
+        public static float cri = 25;
+        public static string playerName = "2hys";
+
+        //
+        public static int gold;
+        public static int level;
+        public static float exp;
+
+    }
+
+    // item
+    public class Item
+    {
+
+    }
+}
